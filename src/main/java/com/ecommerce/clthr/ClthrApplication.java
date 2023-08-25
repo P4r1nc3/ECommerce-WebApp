651606816e6785b87b625d24920b8112a95328c1
parince <konrad.tupta@gmail.com>
2023-08-25 15:08:51 +0200

    Initial Commit

---
+++ src/main/java/com/ecommerce/clthr/ClthrApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.clthr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClthrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClthrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/clthr/ClthrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.clthr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClthrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
