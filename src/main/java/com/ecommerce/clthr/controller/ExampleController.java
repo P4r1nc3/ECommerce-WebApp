70ec312bd3d957145316b2cd0ef299c41355d583
parince <konrad.tupta@gmail.com>
2023-08-28 11:00:25 +0200

    Adjusted ExampleController

--- src/main/java/com/ecommerce/clthr/controller/ExampleController.java
+++ src/main/java/com/ecommerce/clthr/controller/ExampleController.java
@@ -1,5 +1,8 @@
 package com.ecommerce.clthr.controller;
 
+import com.ecommerce.clthr.dto.ProductRepository;
+import com.ecommerce.clthr.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,11 +10,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ExampleController {
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @GetMapping("/")
     public String exampleView(Model model) {
-        String name = "Konrad";
-        model.addAttribute("name", name);
-        System.out.println("from the showExampleView() ");
+        Product p = new Product();
+        Long id = (long) 1;
+        p = productRepository.findById(id).get();
+        model.addAttribute("product", p);
         return "example";
     }
 }
