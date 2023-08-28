c4046a132d6b3724ff4ed4097096622710839f08
parince <konrad.tupta@gmail.com>
2023-08-28 18:02:16 +0200

    Created new endpoint inside ProductController

--- src/main/java/com/ecommerce/clthr/controller/ProductController.java
+++ src/main/java/com/ecommerce/clthr/controller/ProductController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ProductController {
@@ -13,11 +15,30 @@ public class ProductController {
     private ProductRepository productRepository;
 
     @GetMapping("/listProducts")
-    public String exampleView(Model model) {
+    public String showExampleView(Model model) {
         Product p = new Product();
         Long id = (long) 1;
         p = productRepository.findById(id).get();
         model.addAttribute("product", p);
         return "listProducts";
+    }
+
+    @GetMapping("/")
+    public String showAddProduct() {
+        return "addProduct";
+    }
+
+    @PostMapping ("/addP")
+    public String saveProduct(@RequestParam("pname") String name,
+                              @RequestParam("price") int price,
+                              @RequestParam("desc") String description) {
+        Product p = new Product();
+        p.setName(name);
+        p.setPrice(price);
+        p.setDescription(description);
+
+        productRepository.save(p);
+
+        return "redirect:/listProducts";
     }
 }
