1392dab0851332d00c3c919ec26cee819a635c35
parince <konrad.tupta@gmail.com>
2023-08-28 17:56:52 +0200

    Set GenerationType as AUTO

--- src/main/java/com/ecommerce/clthr/model/Product.java
+++ src/main/java/com/ecommerce/clthr/model/Product.java
@@ -2,12 +2,13 @@ package com.ecommerce.clthr.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Product {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
     private int price;
