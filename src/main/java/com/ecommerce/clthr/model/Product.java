8cf2616399d6d5a6f10cbab14c637ecb5ef1f15c
parince <konrad.tupta@gmail.com>
2023-08-29 22:34:24 +0200

    Added image field to the Product model

--- src/main/java/com/ecommerce/clthr/model/Product.java
+++ src/main/java/com/ecommerce/clthr/model/Product.java
@@ -1,9 +1,6 @@
 package com.ecommerce.clthr.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Product {
@@ -13,6 +10,9 @@ public class Product {
     private String name;
     private int price;
     private String description;
+    @Lob
+    @Column(columnDefinition = "MEDIUMBLOB")
+    private String image;
 
     public Long getId() {
         return id;
@@ -44,5 +44,13 @@ public class Product {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
     }
 }
