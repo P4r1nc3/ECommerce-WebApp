972035735741300f15b8534480d431b53b7f7ca8
parince <konrad.tupta@gmail.com>
2023-09-06 22:39:56 +0200

    Created new function

--- src/main/resources/static/api/ajaxFetch.js
+++ src/main/resources/static/api/ajaxFetch.js
@@ -1,6 +1,33 @@
 /**
  *
  */
+function handleListProduct(event)
+{
+    event.preventDefault();
+    fire_ajax1();
+}
+function  fire_ajax1()
+{
+    $.ajax({
+        type: "GET",
+        url: "/products",
+        processData: false,// prevent jQuery from automatically
+        // transforming the data into a query string
+        contentType: false,
+        cache: false,
+        timeout: 600000,
+        success: function (data) {
+             $("#app").empty();
+             $("#app").append(data);
+        },
+        error: function (e) {
+
+            console.log("ERROR : ", e);
+
+        }
+    });
+}
+
 function handleAddProduct(event)
 {
     event.preventDefault();
@@ -17,8 +44,8 @@ function  fire_ajax()
         cache: false,
         timeout: 600000,
         success: function (data) {
-             $("#app").empty();
-             $("#app").append(data);
+            $("#app").empty();
+            $("#app").append(data);
         },
         error: function (e) {
 
