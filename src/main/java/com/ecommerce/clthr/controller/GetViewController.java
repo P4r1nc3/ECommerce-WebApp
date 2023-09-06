dc5e2003d6604def6450a54763ae6227ccc6adee
parince <konrad.tupta@gmail.com>
2023-09-06 22:39:36 +0200

    Added new endpoint

--- src/main/java/com/ecommerce/clthr/controller/GetViewController.java
+++ src/main/java/com/ecommerce/clthr/controller/GetViewController.java
@@ -1,5 +1,8 @@
 package com.ecommerce.clthr.controller;
 
+import com.ecommerce.clthr.dto.ProductRepository;
+import com.ecommerce.clthr.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,15 +10,29 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 public class GetViewController {
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @RequestMapping(value = "/addProduct", method = RequestMethod.GET)
     @ResponseBody
-    public ModelAndView returnExampleView() {
+    public ModelAndView returnAddProduct() {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("addProduct");
-        // mv.addObject("var", "Konrad");
+        return mv;
+    }
+
+    @RequestMapping(value = "/products", method = RequestMethod.GET)
+    @ResponseBody
+    public ModelAndView returnListProducts() {
+        ModelAndView mv = new ModelAndView();
+        List<Product> products = productRepository.findAll();
+        mv.setViewName("listProducts");
+        mv.addObject("products", products);
         return mv;
     }
 }
